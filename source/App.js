ff7c8f2516833060b77429e232a70cac5399755e
metaperl <metaperl@gmail.com>
2014-01-06 09:14:42 -0500

    addorg

--- source/App.js
+++ source/App.js
@@ -1,17 +1,61 @@
 enyo.kind({
-	name: "App",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+  name: "Tweet",
+  kind: enyo.Control,
+  tag: "div",
+  style: "border-style: solid; border-width: 2px; " +
+         "padding: 10px; margin: 10px; min-height: 50px",
+
+  published: {
+    icon: "",
+    handle: "",
+    text: ""
+  },
+
+  components: [
+    { tag: "img", name: "icon",
+      style: "width: 50px; height: 50px; float: left; padding-right: 10px" },
+    { tag: "b", name: "handle" },
+    { tag: "span", name: "text" }
+  ],
+
+  create: function() {
+    this.inherited(arguments);
+    this.iconChanged();
+    this.handleChanged();
+    this.textChanged();
+  },
+
+  iconChanged: function() {
+    this.$.icon.setAttribute("src", this.icon);
+  },
+
+  handleChanged: function() {
+    this.$.handle.setContent(this.handle + ": ");
+  },
+
+  textChanged: function() {
+    this.$.text.setContent(this.text);
+  }
+});
+
+enyo.kind({
+    name: "App",
+    kind: "FittableRows",
+    fit: true,
+    components:[
+        {kind: "onyx.Toolbar", components: [
+            {kind: "onyx.Button", content: "Reload", ontap: "reloadTweets"}
+        ]},
+	{kind: "enyo.Scroller", fit: true, components: [
+	    {name: "main", classes: "nice-padding"}
+	]},
+        {kind: "onyx.TextArea"
+        },
+	{kind: "onyx.Toolbar", components: [
+	    {kind: "onyx.Button", content: "Post Tweet", ontap: "postTweet"}
+	]}
+    ],
+    helloWorldTap: function(inSender, inEvent) {
+	this.$.main.addContent("The button was tapped.<br/>");
+    }
 });
